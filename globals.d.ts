c43210fafc59708c43020ca3c761fc02146eb6b3
Reef <reefmatson@gmail.com>
2023-12-09 11:12:21 +0700

    fix: update warja

--- globals.d.ts
+++ globals.d.ts
@@ -3,12 +3,10 @@ export interface GridInput {
   link: string | null;
   image: string;
   desc: string;
-  'additional-links'?: [
-    {
-      text?: string;
-      icon?: JSX.Element;
-      url: string;
-    },
-  ];
+  'additional-links'?: {
+    text?: string;
+    icon?: JSX.Element;
+    url: string;
+  }[];
   tags?: string[];
 }
